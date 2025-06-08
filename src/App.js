18f5da5ee1466d565264293f7ac576d701d1f1c6
Aditya <bit2swaz@gmail.com>
2025-06-08 12:04:56 +0000

    Add UI/UX micro-animations and page transitions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { CartProvider } from './contexts/CartContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ToastContainer } from 'react-toastify';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
@@ -11,22 +12,49 @@ import CartPage from './pages/CartPage';
 import './styles/global.css';
 import './styles/toast.css';
 
+// Wrapper component for page transitions
+const PageTransition = ({ children }) => {
+  const location = useLocation();
+  
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        timeout={300}
+        classNames="page"
+      >
+        {children}
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+function AppContent() {
+  const location = useLocation();
+  
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <PageTransition>
+          <Routes location={location}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </PageTransition>
+      </main>
+      <ToastContainer position="bottom-right" autoClose={3000} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <ThemeProvider>
         <CartProvider>
-          <div className="App">
-            <Navbar />
-            <main>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/cart" element={<CartPage />} />
-              </Routes>
-            </main>
-            <ToastContainer position="bottom-right" autoClose={3000} />
-          </div>
+          <AppContent />
         </CartProvider>
       </ThemeProvider>
     </Router>
